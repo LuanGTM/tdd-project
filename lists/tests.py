07b3765d35166488c801e895969f2d58e20b11ae
unknown <luangustavo77301@gmail.com>
2026-09-06 20:00:41 -0300

    Utilizando Selenium para Testar Interações do Usuário

--- lists/tests.py
+++ lists/tests.py
@@ -1,17 +1,38 @@
-from django.urls import resolve
-from django.test import TestCase
-from django.http import HttpRequest
-from lists.views import home_page
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import unittest
 
-class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+        inputbox.get_attribute('placeholder'),
+        'Enter a to-do item'
+        )
+        inputbox.send_keys('Estudar testes funcionais')
+        inputbox.send_keys(Keys.ENTER)
+        WebDriverWait(self.browser, 5).until(
+        lambda driver: '1: Estudar testes funcionais'
+        in driver.find_element(By.ID, 'id_list_table').text
+        )
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+        any(
+        row.text == '1: Estudar testes funcionais'
+        for row in rows
+        )
+        )
+if __name__ == '__main__':
+    unittest.main()
+    
