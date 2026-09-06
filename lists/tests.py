3bd15cfcd9139d591dc5492675288b15b05844c9
unknown <luangustavo77301@gmail.com>
2026-09-06 20:12:16 -0300

    Melhorando a Página Inicial

--- lists/tests.py
+++ lists/tests.py
@@ -2,4 +2,12 @@ from django.test import TestCase
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+        table = response.context['table']
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(
+                row.text == '1: Estudar testes funcionais'
+                for row in rows
+            ),
+            'New to-do item did not appear in table'
+        )
