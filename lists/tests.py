88074d699f6e3aae681d44a3dc2314c9f2fac150
unknown <luangustavo77301@gmail.com>
2026-09-06 20:05:36 -0300

    Utilizando o Django Test Client

--- lists/tests.py
+++ lists/tests.py
@@ -1,38 +1,5 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-import unittest
-
-class NewVisitorTest(unittest.TestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-    def tearDown(self):
-        self.browser.quit()
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
-        self.assertIn('To-Do', header_text)
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertEqual(
-        inputbox.get_attribute('placeholder'),
-        'Enter a to-do item'
-        )
-        inputbox.send_keys('Estudar testes funcionais')
-        inputbox.send_keys(Keys.ENTER)
-        WebDriverWait(self.browser, 5).until(
-        lambda driver: '1: Estudar testes funcionais'
-        in driver.find_element(By.ID, 'id_list_table').text
-        )
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-        any(
-        row.text == '1: Estudar testes funcionais'
-        for row in rows
-        )
-        )
-if __name__ == '__main__':
-    unittest.main()
-    
+from django.test import TestCase
+class HomePageTest(TestCase):
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
